7efddc3747c3dbffa4f743b1cdc72177fb24b4ae
Ancil Media <akhil7ancilmedia@gmail.com>
2025-06-30 10:11:32 +0530

    ancil

--- lib/environmental variables.dart
+++ lib/environmental variables.dart
@@ -1,3 +1,5 @@
 
 
-String baseUrl = 'https://panel-backend-sable.vercel.app';
+// String baseUrl = 'https://panel-backend-sable.vercel.app';
+
+String baseUrl = 'panel-backend-git-akhil-ancilmedias-projects.vercel.app';
