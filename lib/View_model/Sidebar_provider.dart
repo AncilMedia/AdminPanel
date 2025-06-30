68a8bfda1db9946bf889cdcd6494ce7292708535
Ancil Media <akhil7ancilmedia@gmail.com>
2025-06-30 10:23:37 +0530

    ancil

--- lib/View_model/Sidebar_provider.dart
+++ lib/View_model/Sidebar_provider.dart
@@ -1,28 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// class SidebarProvider with ChangeNotifier {
-//   String _selectedRoute = '/home';
-//   String _selectedAppSubRoute = '';
-//
-//   String get selectedRoute => _selectedRoute;
-//   String get selectedAppSubRoute => _selectedAppSubRoute;
-//
-//   void selectRoute(String route) {
-//     _selectedRoute = route;
-//     // Reset sub-route when main route changes
-//     if (route != '/apps') {
-//       _selectedAppSubRoute = '';
-//     }
-//     notifyListeners();
-//   }
-//
-//   void selectAppSubRoute(String subRoute) {
-//     _selectedAppSubRoute = subRoute;
-//     notifyListeners();
-//   }
-// }
-
-// SubDrawerProvider.dart
 import 'package:flutter/material.dart';
 
 enum SubDrawerItem { mobile, tv, push , home }
--- lib/environmental variables.dart
+++ lib/environmental variables.dart
@@ -1,5 +1,4 @@
 
 
 // String baseUrl = 'https://panel-backend-sable.vercel.app';
-
 String baseUrl = 'panel-backend-git-akhil-ancilmedias-projects.vercel.app';
